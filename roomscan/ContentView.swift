c583ebad972845e6a84af8e0b45655b50d6d7247
Jeff Nguyen <jeff@nustechnology.com>
2026-07-21 09:02:21 +0700

    Initial Commit

---
+++ roomscan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  roomscan
+//
+//  Created by Thắm Trần on 21/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ roomscan/roomscanApp.swift
@@ -0,0 +1,17 @@
+//
+//  roomscanApp.swift
+//  roomscan
+//
+//  Created by Thắm Trần on 21/7/26.
+//
+
+import SwiftUI
+
+@main
+struct roomscanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
