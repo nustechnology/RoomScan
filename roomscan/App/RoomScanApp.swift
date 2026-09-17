5ecefff04aeaadf1de98149b46fb0a327a22e2e0
ThamTTH <thamtth@nustechnology.com>
2026-09-17 16:12:31 +0700

    Refactor RoomScanApp by removing the AppDelegate and integrating UIApplicationDelegateAdaptor directly into the main app structure.

--- roomscan/App/RoomScanApp.swift
+++ roomscan/App/RoomScanApp.swift
@@ -6,18 +6,8 @@
 import Foundation
 import SwiftUI
 
-final class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
-        return true
-    }
-}
-
 @main
 struct RoomScanApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @Environment(\.scenePhase) private var scenePhase
     @State private var appState: AppState
     @State private var projectsService: any ProjectsService
